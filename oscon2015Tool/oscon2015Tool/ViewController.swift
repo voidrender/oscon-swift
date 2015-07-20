35e60cbda12f71d81137d0dc9e8dae31a0c86cb4
Isaac Overacker <isaac@overacker.me>
2015-07-20 16:08:56 -0700

    Added a label and button to the UI

--- oscon2015Tool/oscon2015Tool/ViewController.swift
+++ oscon2015Tool/oscon2015Tool/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var osconLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(sender: UIButton) {
+        self.osconLabel.text = "Hi there"
+    }
 
 }
 
