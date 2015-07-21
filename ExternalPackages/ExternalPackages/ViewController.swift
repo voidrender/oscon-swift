a5d3d472468e4ce544be90643bd453e0fa5d0d1d
Isaac Overacker <isaac@overacker.me>
2015-07-21 16:09:13 -0700

    Added SQLite.swift example

--- ExternalPackages/ExternalPackages/ViewController.swift
+++ ExternalPackages/ExternalPackages/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import SQLite
 
 class ViewController: UIViewController {
     
@@ -54,6 +55,42 @@ class ViewController: UIViewController {
             .responseString(encoding: nil, completionHandler: { (request, response, string, error) -> Void in
                 println("received: \(string)")
             })
+        
+        // SQLite.swift example
+        let db = Database()
+
+        // Begin constructing a query for users, which will be used to create the table.
+        let users = db["users"]
+        
+        // Columns for the users table
+        let id = Expression<Int64>("id")
+        let name = Expression<String>("name")
+        let email = Expression<String>("email")
+        
+        // Create the users table
+        db.create(table: users, ifNotExists: true) { t in
+            t.column(id, primaryKey: true)
+            t.column(name)
+            t.column(email, unique: true)
+        }
+        
+        // Insert
+        var alice : Query?
+        if let rowID = users.insert(name <- "Alice", email <- "alice@mac.com").rowid { // The <- operator is defined by SQLite.swift
+            alice = users.filter(id == rowID)
+        }
+        
+        // Update
+        alice?.update(email <- "alice@gmail.com")
+        
+        // Query by iteration
+        for user in users {
+            println("User: \(user[id]), name: \(user[name])")
+        }
+        
+        // Query with raw SQL
+        let query = db.prepare("SELECT id, email FROM users")
+        query.run()
     }
 
     override func didReceiveMemoryWarning() {
