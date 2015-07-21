d2790a49e5464866c4810f0e38bd8859a1be1d8a
Isaac Overacker <isaac@overacker.me>
2015-07-21 15:41:41 -0700

    Added POST using alamofire

--- ExternalPackages/ExternalPackages/ViewController.swift
+++ ExternalPackages/ExternalPackages/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        println("GET data")
         Alamofire.request(.GET, "https://placekitten.com/g/500/500")
             .response( { (request, response, data, error) -> Void in
                 switch data {
@@ -25,11 +26,18 @@ class ViewController: UIViewController {
                 }
             })
         
+        println("GET JSON")
         Alamofire.request(.GET, "https://httpbin.org/get")
             .responseJSON(options: NSJSONReadingOptions.allZeros) { (request, response, data, error) -> Void in
                 if let dictionary = data as? NSDictionary {
                     println("I loaded a dictionary! \(dictionary)")
                 }
+        }
+        
+        println("POST")
+        Alamofire.request(.POST, "https://httpbin.org/post", parameters: ["foo": "bar"], encoding: .JSON)
+            .responseJSON(options: nil) { (request, response, responseObject, error) -> Void in
+                println("Received \(responseObject)")
         }
     }
 
