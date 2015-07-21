69b540204d1f521a0c1fa41ff6b0bf2598509f4e
Isaac Overacker <isaac@overacker.me>
2015-07-21 12:04:26 -0700

    Completed fingerprint scanner example

--- FingerprintScanner/FingerprintScanner/ViewController.swift
+++ FingerprintScanner/FingerprintScanner/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    
+    let authenticationContext = LAContext()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func authenticate(sender: AnyObject) {
+        let policy = LAPolicy.DeviceOwnerAuthenticationWithBiometrics
+        
+        var error : NSError? = nil
+        let canAuthenticate = authenticationContext.canEvaluatePolicy(policy, error: &error)
+        if canAuthenticate == false {
+            println("Cannot authenticate: \(error)")
+            return
+        }
+        
+        let reason = "You're about to spend money"
+        
+        authenticationContext.evaluatePolicy(policy, localizedReason: reason) { (succeeded, error) -> Void in
+            if succeeded {
+                let alert = UIAlertController(title: "Success!", message: "Biometric scanning succeeded. Money spent.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+            else {
+                let alert = UIAlertController(title: "Failure!", message: "Biometric scanning failed.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        }
+    }
 
 }
 
